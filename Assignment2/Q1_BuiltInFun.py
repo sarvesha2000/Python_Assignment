46ee9d40c0206ff2e0fadf0f41ee2f6247daa3ba
sarvesha2000 <sarveshapathak2000@gmail.com>
2026-06-15 16:23:48 +0530

    Added Assignment 2

---
+++ Assignment2/Q1_BuiltInFun.py
@@ -0,0 +1,12 @@
+'''
+1.write a program to display:
+1.value
+2.type
+3.Memory address for a variable using appropriate built-in function
+'''
+x= 10
+
+print("Value =",x)
+print("Type =",type(x))
+print("Memory Address =",id(x))
+
---
+++ Assignment2/Q9_Age.py
@@ -0,0 +1,12 @@
+'''
+Write a program to take user's name and age and display
+Hello<name> ,you will tern<age + 1> next year
+'''
+
+print("Enter your name :")
+Name =input()
+
+print("Enter you age :")
+Age =int(input())
+
+print("hello",Name,"You will turn",Age + 1,"Next year")
