4818d2d7283c8d43b6dad6c7706ebde8c2a60d9f
sarvesha2000 <sarveshapathak2000@gmail.com>
2026-06-22 17:23:56 +0530

    Added Assignment 3

---
+++ Assignment 3/datatype.py
@@ -0,0 +1,17 @@
+
+'''
+write a program to display
+data type
+memory address
+size in bytes
+of variable entred by user
+'''
+from sys import getsizeof
+
+x ="Sarvesha"
+
+print(type(x))
+print(id(x))
+print(getsizeof(x))
+
+
